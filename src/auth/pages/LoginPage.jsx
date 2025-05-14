0568917b489c636466abe86b3ffdcaadabaf9ee0
José Bolaños <84158344+jbolanosa@users.noreply.github.com>
2025-05-14 12:06:45 -0600

    navbar

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,9 +1,15 @@
+import { useContext } from "react";
 import { useNavigate } from "react-router"
+import { AuthContext } from "../context";
 
 export const LoginPage = () => {
+
+  const {login} = useContext(AuthContext);
   const navigate = useNavigate();
 
   const onLogin = () => {
+
+    login('José Bolaños');
     navigate('/', {
       replace: true
     })
